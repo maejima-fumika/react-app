b9eeadd4fd184c873932c8e3d08e3bde119d2758
maejima_fumika <fumi.maejima1112@gmail.com>
2021-04-24 21:56:52 +0900

    勝者を決められるようになった

--- src/__test__/components/Board.test.js
+++ src/__test__/components/Board.test.js
@@ -2,23 +2,55 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Board from '../../components/Board';
 
-test('renders board component', async () => {
+test('exist button', async () => {
   render(<Board />)
   let test_squares = Array(9).fill("")
+  //全てのボタンが存在して、ちゃんと空か
   for (let i = 0; i < 9; i++) {
     expect(screen.getByTestId("square" + String(i))).toBeInTheDocument()
     expect(screen.getByTestId("square" + String(i)).innerHTML).toBe(test_squares[i])
   }
-  expect(screen.getByText("Next player: X")).toBeInTheDocument();
-  await userEvent.click(screen.getByTestId("square0"))
-  test_squares[0] = "X"
+});
+
+test('can judge winner1', async () => {
+  render(<Board />)
+  const inputOrder = [0,4,1,2,6,7,3]
+  const tmp_board = await clickButtons(inputOrder)
   for (let i = 0; i < 9; i++) {
-    expect(screen.getByTestId("square" + String(i)).innerHTML).toBe(test_squares[i])
-  }
-  expect(screen.getByText("Next player: O")).toBeInTheDocument();
-  await userEvent.click(screen.getByTestId("square1"))
-  test_squares[1] = "O"
-  for (let i = 0; i < 9; i++) {
-    expect(screen.getByTestId("square" + String(i)).innerHTML).toBe(test_squares[i])
+    expect(screen.getByTestId("square" + String(i)).innerHTML).toBe(tmp_board[i])
   }
 });
+
+test('can judge winner2', async () => {
+  render(<Board />)
+  const inputOrder = [6,4,8,2,7]
+  const tmp_board = await clickButtons(inputOrder)
+  for (let i = 0; i < 9; i++) {
+    expect(screen.getByTestId("square" + String(i)).innerHTML).toBe(tmp_board[i])
+  }
+});
+
+test('can judge draw', async () => {
+  render(<Board />)
+  const inputOrder = [0,4,1,2,6,7,8,3,5]
+  const tmp_board = await clickButtons(inputOrder)
+  for (let i = 0; i < 9; i++) {
+    expect(screen.getByTestId("square" + String(i)).innerHTML).toBe(tmp_board[i])
+  }
+});
+
+
+function clickButtons(inputOrder) {
+  return new Promise( async (resolve)=>{
+  let test_squares = Array(9).fill("")
+  for (let i = 0; i < inputOrder.length; i++) {
+    expect(screen.getByText('Next player: ' + (i%2 ? 'O' : 'X'))).toBeInTheDocument();
+    await userEvent.click(screen.getByTestId("square" + String(inputOrder[i])))
+    test_squares[inputOrder[i]] = i%2 ? 'O' : 'X'
+    for (let j = 0; j < 9; j++) {
+      expect(screen.getByTestId("square" + String(j)).innerHTML).toBe(test_squares[j])
+    }
+  }
+  resolve(test_squares)
+})
+}
